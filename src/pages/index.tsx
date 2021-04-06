d98327a784eae49c2e776187d3f84444289ad3bf
Felipe de Abreu Ferrarini <ferrarinifelipe@gmail.com>
2021-04-05 22:29:33 -0300

    #5 formulários e validação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
 import { Button, Flex, Stack } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useRouter } from 'next/dist/client/router';
 import { Input } from '../components/Form/Input';
 
+type SignInFormData = {
+  email: string;
+  password: string;
+  loginError?: string;
+};
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória'),
+});
+
 export default function SignIn(): JSX.Element {
+  const router = useRouter();
+
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
+
+  const handleSingIn: SubmitHandler<SignInFormData> = async values => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    router.push('/dashboard');
+  };
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -12,12 +39,34 @@ export default function SignIn(): JSX.Element {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSingIn)}
       >
         <Stack>
-          <Input id="email" name="email" type="email" label="E-mail" />
-          <Input id="password" name="password" type="password" label="Senha" />
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            label="E-mail"
+            error={formState.errors.email}
+            {...register('email')}
+          />
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            label="Senha"
+            error={formState.errors.password}
+            {...register('password')}
+          />
         </Stack>
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+
+        <Button
+          isLoading={formState.isSubmitting}
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+        >
           Entrar
         </Button>
       </Flex>
--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -9,18 +9,62 @@ import {
   VStack,
 } from '@chakra-ui/react';
 import Link from 'next/link';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { Input } from '../../components/Form/Input';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 
+type CreateUserFormData = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+};
+
+const createUserFormSchema = yup.object().shape({
+  name: yup.string().required('Nome obrigatório'),
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup
+    .string()
+    .required('Senha obrigatória')
+    .min(6, 'No mínimo 6 caracteres'),
+  password_confirmation: yup
+    .string()
+    .oneOf([null, yup.ref('password')], 'As senhas precisam ser iguais'),
+});
+
 export default function CreateUser(): JSX.Element {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: yupResolver(createUserFormSchema),
+  });
+
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async values => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    // eslint-disable-next-line no-console
+    console.log(values);
+  };
+
   return (
     <Box>
       <Header />
       <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius="8" bg="gray.800" p={['6', '8']}>
+        <Box
+          as="form"
+          flex="1"
+          borderRadius="8"
+          bg="gray.800"
+          p={['6', '8']}
+          onSubmit={handleSubmit(handleCreateUser)}
+        >
           <Heading size="lg" fontWeight="normal">
             Criar usuário
           </Heading>
@@ -29,16 +73,35 @@ export default function CreateUser(): JSX.Element {
 
           <VStack spacing="8">
             <SimpleGrid minChildWidth="240px" spacing={['6', '8']} w="100%">
-              <Input name="name" label="Nome completo" />
-              <Input name="email" label="E-mail" type="email" />
+              <Input
+                name="name"
+                label="Nome completo"
+                error={errors.name}
+                {...register('name')}
+              />
+              <Input
+                name="email"
+                label="E-mail"
+                type="email"
+                error={errors.email}
+                {...register('email')}
+              />
             </SimpleGrid>
 
             <SimpleGrid minChildWidth="240px" spacing={['6', '8']} w="100%">
-              <Input name="password" label="Senha" type="password" />
+              <Input
+                name="password"
+                label="Senha"
+                type="password"
+                error={errors.password}
+                {...register('password')}
+              />
               <Input
                 name="password_confirmation"
                 label="Confirmação da Senha"
                 type="password"
+                error={errors.password_confirmation}
+                {...register('password_confirmation')}
               />
             </SimpleGrid>
           </VStack>
@@ -54,7 +117,9 @@ export default function CreateUser(): JSX.Element {
                   Cancelar
                 </Button>
               </Link>
-              <Button colorScheme="pink">Salvar</Button>
+              <Button isLoading={isSubmitting} type="submit" colorScheme="pink">
+                Salvar
+              </Button>
             </Stack>
           </Flex>
         </Box>
